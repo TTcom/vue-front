a73429763882bb8495625c5220a98b2d1181dd7a
ttcom <1902305073@qq.com>
2019-10-24 16:57:17 +0800

    firstcommit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png">
+		<ul>
+			<li v-for="item in users">{{item.name}}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import axios from 'axios'
+	export default {
+		name: 'app',
+		data(){
+			return{
+				users:[]
+			}
+		},
+		mounted(){
+			axios.get('http://localhost:3000/api/users').then(res=>{
+				 this.users = res.data;
+				 
+			})
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+
+
+const vueConfig = {
+	assetsDir: 'assets',//指定静态文件输出目录(相对于outputDir)
+    lintOnSave:false,      //是否关闭eslint校验
+}
+module.exports = vueConfig;
